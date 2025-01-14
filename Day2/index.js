c80583a65ed7546cb47d4f3cfc82a2e73417fce1
PNS212624\Hansol <yrlee00@hansol.com>
2025-01-14 12:42:22 +0900

    Day2 과제 제출

---
+++ Day2/index.js
@@ -0,0 +1,29 @@
+// 미션 1 switch case문 작성하기
+let animal = 'cat';
+
+switch (animal) {
+    case 'cat':
+        console.log('고양이');
+        break;
+    case 'dog':
+        console.log('개');
+        break;
+    case 'tiger':
+        console.log('호랑이');
+        break;
+    case 'lion':
+        console.log('사자');
+        break;
+}
+
+// 미션 2 함수 작성하기
+let answer = '';
+
+function connenctStrings(str1, str2) {
+    answer = str1 + str2;
+    return answer;
+}
+
+connenctStrings('hello', 'world');
+
+console.log(answer);
