ccbe86407ef2194f0a1e86be3479e5dc11272663
PNS212624\Hansol <yrlee00@hansol.com>
2025-02-03 10:08:49 +0900

    feat: 컴포넌트 분리

---
+++ Animal/App.js
@@ -0,0 +1,7 @@
+import TabBar from './components/TabBar.js';
+import Content from './components/Content.js';
+
+export default function App($app) {
+    const tabBar = new TabBar();
+    const content = new Content();
+}
--- Animal/index.js
+++ Animal/index.js
@@ -1,22 +1,4 @@
-const API_URL = 'https://animal-api-two.vercel.app/';
+import App from './App.js';
 
-const $content = document.querySelector('div.content');
-let template = [];
-
-const getData = async () => {
-    let res = await fetch(API_URL);
-
-    try {
-        if (res) {
-            let data = await res.json();
-            data.photos.forEach((elm) => {
-                template += `<img src=${elm.url}></img>`;
-            });
-            $content.innerHTML = template;
-        }
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-getData();
+const $app = document.getElementById('app');
+new App($app);
